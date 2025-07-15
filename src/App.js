3d2fb5360ceacbbc29b59babc90902b1dce31c5d
jsholeen <149888719+jsholeen@users.noreply.github.com>
2025-07-15 16:57:45 -0500

    finally

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import GeneralTopics from './sections/GeneralTopics';
 import SurvivalGuideEMR from './sections/SurvivalGuideEMR';
@@ -14,34 +14,31 @@ import Neo from './sections/neo';
 import Peds from './sections/peds';
 import Im from './sections/im';
 import Surgery from './sections/surgery';
-import './App.css'
+
 
 
 function App() {
   return (
     <Router>
-      <div >
-        
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/GeneralTopics" element={<GeneralTopics />} />
+        <Route path="/SurvivalGuideEMR" element={<SurvivalGuideEMR />} />
+        <Route path="/Labs" element={<Labs />} />
+        <Route path="/Neurology" element={<Neurology />} />
+        <Route path="/Psychiatry" element={<Psychiatry />} />
+        <Route path="/Labor" element={<Labor />} />
+        <Route path="/ob" element={<Ob />} />
+        <Route path="/gyn" element={<Gyn />} />
+        <Route path="/family" element={<Family />} />
+        <Route path="/phm" element={<PHM />} />
+        <Route path="/neo" element={<Neo />} />
+        <Route path="/peds" element={<Peds />} />
+        <Route path="/im" element={<Im />} />
+        <Route path="/surgery" element={<Surgery />} />
 
-        {/* Page content */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/GeneralTopics" element={<GeneralTopics />} />
-          <Route path="/SurvivalGuideEMR" element={<SurvivalGuideEMR />} />
-          <Route path="/Labs" element={<Labs />} />
-          <Route path="/Neurology" element={<Neurology />} />
-          <Route path="/Psychiatry" element={<Psychiatry />} />
-          <Route path="/Labor" element={<Labor />} />
-          <Route path="/ob" element={<Ob />} />
-          <Route path="/gyn" element={<Gyn />} />
-          <Route path="/family" element={<Family />} />
-          <Route path="/phm" element={<PHM />} />
-          <Route path="/neo" element={<Neo />} />
-          <Route path="/peds" element={<Peds />} />
-          <Route path="/im" element={<Im />} />
-          <Route path="/surgery" element={<Surgery />} />
-        </Routes>
-      </div>
+        {/* Add up to Section16 */}
+      </Routes>
     </Router>
   );
 }
