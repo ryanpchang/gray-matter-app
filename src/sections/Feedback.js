d82b3d44894057c8dc30779b703708256362996e
jsholeen <149888719+jsholeen@users.noreply.github.com>
2025-07-17 22:14:08 -0500

    added imaging and feedback to home

---
+++ src/sections/Feedback.js
@@ -0,0 +1,12 @@
+import SectionTemplate from '../components/SectionTemplate';
+
+const Feedback = () => {
+  return (
+    <SectionTemplate
+      title = "Feedback"
+      content= "This is the text content for the Feedback section. Please provide your feedback here."
+   />
+  );
+};
+
+export default Feedback;
